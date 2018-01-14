0d14116ebe7d16d3c4f5395376a5af47b9dee501
takahikomaru <34597731+takahikomaru@users.noreply.github.com>
2018-01-14 10:47:50 +0900

    グリッドボタン

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,26 +1,42 @@
 package application;
-	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("Test");
+
+        Button btn1 = new  Button("　");
+        Button btn2 = new  Button("　");
+        Button btn3 = new  Button("　");
+        Button btn4 = new  Button("　");
+        Button btn5 = new  Button("　");
+        Button btn6 = new  Button("　");
+        Button btn7 = new  Button("　");
+        Button btn8 = new  Button("　");
+        Button btn9 = new  Button("　");
+
+        GridPane pane = new GridPane();
+        pane.add(btn1, 0, 0);
+        pane.add(btn2, 1, 0);
+        pane.add(btn3, 2, 0);
+        pane.add(btn4, 0, 1);
+        pane.add(btn5, 1, 1);
+        pane.add(btn6, 2, 1);
+        pane.add(btn7, 0, 2);
+        pane.add(btn8, 1, 2);
+        pane.add(btn9, 2, 2);
+
+        btn1.setOnAction(event -> btn1.setText("〇"));
+
+        Scene scene = new Scene(pane,100,100);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
